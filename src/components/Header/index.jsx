e2e6876a719999b1f918238a629ef92d9f0bbe5b
funzd <funzdpro1@gmail.com>
2023-04-11 23:47:23 -0600

    improving styles.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
-
+// QUITAR ESOS GRID Y CENTRAR EL TASK /
 const Header = () =>( 
-  <div className="text-center grid grid-rows-1 grid-cols-5 justify-items-center gap-2">
-    <div className="col-span-4">
-      Tasks!
+  <div className="static">
+    <div className="w-99">
+      <Link href="/">
+        <h1 className="text-center font-extrabold hover:font-thin text-2xl">
+          Tasks!
+        </h1>
+      </Link>
     </div> 
-    <div className="text-center">
-     <a href="#">
+    <div className="absolute top-0 right-0 mr-2">
+    <a href="#">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-</svg>
-  </a>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+      </svg>
+    </a>
     </div>
   </div>
 )
