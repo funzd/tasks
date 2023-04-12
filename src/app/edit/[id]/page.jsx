864f1dcecb8eff12a588ac887b21489787311aea
funzd <funzdpro1@gmail.com>
2023-04-11 23:49:45 -0600

    refactor edit/.

--- src/app/edit/[id]/page.jsx
+++ src/app/edit/[id]/page.jsx
@@ -1,8 +1,7 @@
-"use client"
-import {useTasks} from '../../../context/taskContext.jsx'
 const Page = ({params}) => {
-  const values = useTasks()
-  console.log(values)
- return  <h1>Editando {params.id}</h1>
+ return <h1>
+          Editando {params.id}
+        </h1>
 }
+
 export default Page
