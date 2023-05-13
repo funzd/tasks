b7343645203c87b1f9b0870e6583da93ac4854fb
funzd <funzdpro1@gmail.com>
2023-05-13 14:40:15 -0600

    active routes done

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,20 +1,38 @@
-import Link from 'next/link'
+"use client"
 
-const Nav = () =>
-  <nav >
-    <ul className="flex justify-center">
+import Link from 'next/link'
+import {useState} from "react"
+
+const Nav = () =>{
+  const [active, setActive] = useState(true)
+  const style = "font-bold rounded-full bg-cyan-500 p-1"
+  const On = () =>{
+    setActive(true)
+  }
+  const Off = () =>{
+    setActive(false)
+  }
+
+  return <nav  className="border-solid border-b-2 content-center">
+    <ul className="flex justify-center m-2">
       <li className="ml-6 mr-6">
-        <Link className="rounded-full bg-cyan-500 p-1" 
+        <Link 
+          className={(active)?style:"text-gray-500"} 
+          onClick={On}
           href="#">
           Owns 
         </Link>
       </li>
       <li className="ml-6 mr-6">
-       <Link href="#">
+       <Link
+          className={(active)?"text-gray-500":style}
+          onClick={Off}
+          href="#">
           Teams  
         </Link>
       </li>
     </ul>
   </nav>
+}
 
 export default Nav
