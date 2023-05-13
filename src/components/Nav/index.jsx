944583d4bebc92a85412567cc621886aabbf4f7d
funzd <funzdpro1@gmail.com>
2023-05-13 11:43:04 -0600

    Navigation created

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,13 +1,19 @@
 import Link from 'next/link'
+
 const Nav = () =>
-  <nav>
-    <ul className="flex gap-4">
-      <Link href="#">
-        <button className="rounded-full bg-red-300 p-1">Owns</button> 
-      </Link>
-      <Link href="#">
-        <li>Teams</li>
-      </Link>
+  <nav >
+    <ul className="flex justify-center">
+      <li className="ml-6 mr-6">
+        <Link className="rounded-full bg-cyan-500 p-1" 
+          href="#">
+          Owns 
+        </Link>
+      </li>
+      <li className="ml-6 mr-6">
+       <Link href="#">
+          Teams  
+        </Link>
+      </li>
     </ul>
   </nav>
 
